a84be5960d7fde6fca576f8e44634905f3e3157b
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2024-04-25 09:10:26 -0300

    - Correções de UI e funcionalidades

--- app/src/main/java/com/example/avaliacaon1/SecretCodeActivity.java
+++ app/src/main/java/com/example/avaliacaon1/SecretCodeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
@@ -37,6 +38,12 @@ public class SecretCodeActivity extends AppCompatActivity {
         n2 = findViewById(R.id.n2);
         n3 = findViewById(R.id.n3);
         n4 = findViewById(R.id.n4);
+
+        n1.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+        n2.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+        n3.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+        n4.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+
 
         n1.setFilters(new InputFilter[] {new FiltroNumeroInput(0, 9)});
         n2.setFilters(new InputFilter[] {new FiltroNumeroInput(0, 9)});
